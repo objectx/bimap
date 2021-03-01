bffcdcc0efceb34fcfd6b671ab0223cadf6fe221
Masashi Fujita <objectxtreme@gmail.com>
2021-03-02 02:45:29 +0900

    refactor: Reduce allocation

--- inc/bimap.hpp
+++ inc/bimap.hpp
@@ -29,10 +29,12 @@ private:
     struct is_transparent<T_, T2_, std::void_t<typename T_::is_transparent>> : std::true_type {};
 
 private:
-    std::vector<value_type>             items_;
-    mutable std::vector<uint32_t>       keys_;
-    mutable std::vector<uint32_t>       values_;
-    mutable bool                        dehydrated_ = false;
+    std::vector<value_type> items_;
+    /// Represents two ordered collections.
+    /// To reduce allocation, we merge two collection into the single array.
+    /// First half (0..<size()) contains sorted key indices.
+    /// Second half (size ()..<(2 * size()) contains sorted value indices.
+    mutable std::unique_ptr<uint32_t[]> indices_;  // NOLINT(modernize-avoid-c-arrays)
     [[no_unique_address]] key_compare   cmp_key_    = KeyComparator_ {};
     [[no_unique_address]] value_compare cmp_value_  = ValueComparator_ {};
 
@@ -44,7 +46,7 @@ public:
     [[nodiscard]] size_t size () const noexcept { return items_.size (); }
 
     self_t &add (value_type &&item) {
-        if (dehydrated_) {
+        if (dehydrated ()) {
             throw std::runtime_error {"attempt to modify the dehydrated instance"};
         }
         items_.template emplace_back (std::move (item));
@@ -54,116 +56,126 @@ public:
     self_t &add (const value_type &item) { return this->add (value_type {item}); }
 
     std::optional<std::reference_wrapper<const V_>> find_value (const K_ &key) const {
-        if (! dehydrated_) {
+        if (! dehydrated ()) {
             dehydrate ();
         }
-        int64_t left  = -1;
-        int64_t right = items_.size ();
+        auto const *keys  = key_indices ();
+        int64_t     left  = -1;
+        int64_t     right = items_.size ();
         while (1 < (right - left)) {
             int64_t mid = left + (right - left) / 2u;
-            if (cmp_key_ (items_[keys_[mid]].first, key)) {
+            if (cmp_key_ (items_[keys[mid]].first, key)) {
                 left = mid;
             }
             else {
                 right = mid;
             }
         }
-        auto const &k = items_[keys_[right]].first;
+        auto const &k = items_[keys[right]].first;
         if (cmp_key_ (key, k) || cmp_key_ (k, key)) {
             return {};
         }
-        return {std::cref (items_[keys_[right]].second)};
+        return {std::cref (items_[keys[right]].second)};
     }
 
     template<typename U_>
     std::enable_if_t<is_transparent<key_compare, U_>::value, std::optional<std::reference_wrapper<const V_>>> find_value (const U_ &key) const {
-        if (! dehydrated_) {
+        if (! dehydrated ()) {
             dehydrate ();
         }
-        int64_t left  = -1;
-        int64_t right = items_.size ();
+        auto const *keys  = key_indices ();
+        int64_t     left  = -1;
+        int64_t     right = items_.size ();
         while (1 < (right - left)) {
             int64_t mid = left + (right - left) / 2u;
-            if (cmp_key_ (items_[keys_[mid]].first, key)) {
+            if (cmp_key_ (items_[keys[mid]].first, key)) {
                 left = mid;
             }
             else {
                 right = mid;
             }
         }
-        auto const &k = items_[keys_[right]].first;
+        auto const &k = items_[keys[right]].first;
         if (cmp_key_ (key, k) || cmp_key_ (k, key)) {
             return {};
         }
-        return {std::cref (items_[keys_[right]].second)};
+        return {std::cref (items_[keys[right]].second)};
     }
 
     std::optional<std::reference_wrapper<const K_>> find_key (const V_ &value) const {
-        if (! dehydrated_) {
+        if (! dehydrated ()) {
             dehydrate ();
         }
-        int64_t left  = -1;
-        int64_t right = items_.size ();
+        auto const *values = value_indices ();
+        int64_t     left   = -1;
+        int64_t     right  = items_.size ();
         while (1 < (right - left)) {
             int64_t mid = left + (right - left) / 2u;
-            if (cmp_value_ (items_[values_[mid]].second, value)) {
+            if (cmp_value_ (items_[values[mid]].second, value)) {
                 left = mid;
             }
             else {
                 right = mid;
             }
         }
-        auto const &v = items_[values_[right]].second;
+        auto const &v = items_[values[right]].second;
         if (cmp_value_ (value, v) || cmp_value_ (v, value)) {
             return {};
         }
-        return {std::cref (items_[values_[right]].first)};
+        return {std::cref (items_[values[right]].first)};
     }
 
     template<typename U_>
     std::enable_if_t<is_transparent<value_compare, U_>::value, std::optional<std::reference_wrapper<const K_>>>
     find_key (const U_ &value) const {
-        if (! dehydrated_) {
+        if (! dehydrated ()) {
             dehydrate ();
         }
-        int64_t left  = -1;
-        int64_t right = items_.size ();
+        auto const *values = value_indices ();
+        int64_t     left   = -1;
+        int64_t     right  = items_.size ();
         while (1 < (right - left)) {
             int64_t mid = left + (right - left) / 2u;
-            if (cmp_value_ (items_[values_[mid]].second, value)) {
+            if (cmp_value_ (items_[values[mid]].second, value)) {
                 left = mid;
             }
             else {
                 right = mid;
             }
         }
-        auto const &v = items_[values_[right]].second;
+        auto const &v = items_[values[right]].second;
         if (cmp_value_ (value, v) || cmp_value_ (v, value)) {
             return {};
         }
-        return {std::cref (items_[values_[right]].first)};
+        return {std::cref (items_[values[right]].first)};
     }
 
     void dehydrate () const {
-        if (dehydrated_) {
+        if (dehydrated()) {
             return;
         }
-        keys_.resize (items_.size ());
-        values_.resize (items_.size ());
+        indices_ = std::make_unique<uint32_t[]> (2u * size ());  // NOLINT(modernize-avoid-c-arrays)
+        auto *k  = &indices_[0];
+        auto *v  = &indices_[size ()];
         for (size_t i = 0; i < items_.size (); ++i) {
-            keys_[i]   = i;
-            values_[i] = i;
+            k[i] = i;
+            v[i] = i;
         }
-        std::sort (keys_.begin (), keys_.end (), [this] (auto const &a, auto const &b) -> bool {
+        std::sort (&k[0], &k[size ()], [this] (auto const &a, auto const &b) -> bool {
             return cmp_key_ (this->items_[a].first, this->items_[b].first);
         });
-        std::sort (values_.begin (), values_.end (), [this] (auto const &a, auto const &b) -> bool {
+        std::sort (&v[0], &v[size ()], [this] (auto const &a, auto const &b) -> bool {
             return cmp_value_ (this->items_[a].second, this->items_[b].second);
         });
-        dehydrated_ = true;
     }
 
-    bool dehydrated () const noexcept { return dehydrated_; }
+    [[nodiscard]] bool dehydrated () const noexcept { return indices_ != nullptr; }
 
-    void hydrate () const { dehydrated_ = false; }
+    void hydrate () const {
+        indices_.reset ();
+    }
+
+private:
+    const uint32_t *key_indices () const { return &indices_[0]; }
+    const uint32_t *value_indices () const { return &indices_[size ()]; }
 };
